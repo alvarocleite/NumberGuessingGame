9851041df39c8617bcdcec99fe3f9ffdcb44da14
Alvaro C Leite <alvaroleite01@gmail.com>
2025-05-01 15:20:41 +0100

    Update .h to .hpp

--- NumberGuessingGame.cpp
+++ NumberGuessingGame.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-#include "cli.h"
+#include "cli.hpp"
 
 #define DIFFICULTYLEVELS 3
 
--- cli.cpp
+++ cli.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "cli.h"
+#include "cli.hpp"
 
 using namespace std;
 
